d8f04765b4fca18f9b15147592a19df22d776841
Adilson Ramos <75184157+AdilsonRamosO@users.noreply.github.com>
2021-01-23 19:23:35 -0300

    Cancelamento e envio de e-mail

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  secure: false,
+  auth: {
+    user: "20b3715a303c3f",
+    pass: "9ed6e1f5e48c54"
+  },
+  default: {
+    from: 'Equipe GoBarber <noreplay@gobarber.com>'
+  }
+}
