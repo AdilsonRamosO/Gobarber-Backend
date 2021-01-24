0ab0baaf4e2e804bc1ff75eb9da9effb72415ed6
Adilson Ramos <75184157+AdilsonRamosO@users.noreply.github.com>
2021-01-23 21:15:33 -0300

    Configurações avançadas

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: "smtp.mailtrap.io",
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: "20b3715a303c3f",
-    pass: "9ed6e1f5e48c54"
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreplay@gobarber.com>'
